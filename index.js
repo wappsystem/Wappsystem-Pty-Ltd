550ebe62c07ed56b618a61c9730d017cee6d36ee
vmiis <zzm.vmiis@gmail.com>
2016-01-24 22:18:35 +1100

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ $vm.module_list={
     upload_supporting_document:  ['20007516','.../modules/upload_supporting_document.html'],
     income_details:              ['--------','.../modules/income_details.html'],
     invoice:                     ['20004121','.../modules/invoice.html'],
-    wp_client:                      ['20000368','.../modules/client.html'],
+    client:                      ['20000368','.../modules/client.html'],
 
 }
 //--------------------------------------------------------
@@ -17,21 +17,26 @@ for(key in $vm.module_list){
     $vm.module_list[key][2]=$vm.module_list[key][2].replace('...','https://github.com/'+$vm.repository+'/master')
 }
 //--------------------------------------------------------
+var jsN=0;
 var last=function(){
     $('head').append("<style> *{ margin:0; } html,body { height:100%;} </style>");
     setTimeout(function () {
         $.ajaxSetup({ cache: true });
-        $.getScript('https://ajax.aspnetcdn.com/ajax/jquery.ui/1.11.4/jquery-ui.min.js');
-        $.getScript('https://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js');
-        $.getScript('https://sdk.amazonaws.com/js/aws-sdk-2.1.34.min.js');
-        $.getScript('https://cbs.wappsystem.com/system/third/handsontable.full.min.js');
-        $.getScript('https://cbs.wappsystem.com/dev/vm.js?v=20160108');
+        $.getScript('https://ajax.aspnetcdn.com/ajax/jquery.ui/1.11.4/jquery-ui.min.js',function(){last_last();});
+        $.getScript('https://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js',function(){last_last();});
+        $.getScript('https://sdk.amazonaws.com/js/aws-sdk-2.1.34.min.js',function(){last_last();});
+        $.getScript('https://cbs.wappsystem.com/system/third/handsontable.full.min.js',function(){last_last();});
+        $.getScript('https://cbs.wappsystem.com/dev/vm.js?v=20160108',function(){last_last();});
         $.getScript('https://www.google.com/jsapi',function(){
-            google.load('visualization', '1', {'packages':['corechart'],callback:function(){} } );
+            google.load('visualization', '1', {'packages':['corechart'],callback:function(){last_last();} } );
         });
         $('head').append("<link rel='stylesheet' media='screen' href='https://cbs.wappsystem.com/system/third/handsontable.full.min.css'>");
         $('head').append("<link rel='stylesheet' href='https://ajax.aspnetcdn.com/ajax/jquery.ui/1.11.4/themes/redmond/jquery-ui.css'>");
-    }, 100);
+    }, 300);
 
+}
+//--------------------------------------------------------
+var last_last=function(){
+    jsN++; if(jsN===6) $vm.load_demo();
 }
 //--------------------------------------------------------
