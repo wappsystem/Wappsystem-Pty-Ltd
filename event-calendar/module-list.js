3836c1074fd56f6534bebf1955f13ff5ca4237b0
Gunnar Unger <gunnar.unger@gmail.com>
2019-06-30 17:56:07 +1000

    update

--- event-calendar/module-list.js
+++ event-calendar/module-list.js
@@ -1,20 +1,19 @@
 (function(){
     //-------------------------------------------------------------------------------------
     var modules={
-        "event-data":      	  		    {url:"$H/m/event-data.html",Table:"event-vm",form_module:"event-form"},
-        "event-form":      	  		    {url:"$H/m/event-form.html",Table:"event-vm"},         
-        "event-daily":      	 	    {url:"$H/m/event-daily.html",Table:"event-vm",router:1,
+        "event-data":      	  		    {url:"$H/m/event-data.html",Table:"event-wappsystem",form_module:"event-form"},
+        "event-form":      	  		    {url:"$H/m/event-form.html",Table:"event-wappsystem"},         
+        "event-daily":      	 	    {url:"$H/m/event-daily.html",Table:"event-wappsystem",router:1,
                                             event_form:"event-form"
                                         },         
-        "event-monthly":      	 	    {url:"$H/m/event-monthly.html",Table:"event-vm",router:1,
+        "event-monthly":      	 	    {url:"$H/m/event-monthly.html",Table:"event-wappsystem",router:1,
                                             event_daily:"event-daily",
                                             event_form:"event-form"
                                         },         
-        "event-panel":      	 	    {url:"$H/m/event-panel.html",Table:"event-vm",router:1,
+        "event-panel":      	 	    {url:"$H/m/event-panel.html",Table:"event-wappsystem",router:1,
                                             event_daily:"event-daily",
                                             form_module:"event-form"
                                         },         
-        "about-vmiis":  {url:"$H/m/about-vmiis.html",router:1},
         "module-editor":{url:"https://vm.vmiis.com/component/m/module-editor.html",router:1,sys:1},
     }
     for(p in modules){
--- invoice-management/module-list.js
+++ invoice-management/module-list.js
@@ -2,12 +2,11 @@
     //-------------------------------------------------------------------------------------
     var modules={
         "workflow":                 {url:"$H/m/invoice-management-workflow.html",router:1,name:"Invoice management",tags:"invoice,finance"},
-        "invoice-data": 	        {url:"$H/m/invoice-data.html",Table:"invoice-vm",form_module:"invoice-form",router:1},
-        "invoice-form": 	        {url:"$H/m/invoice-form.html",Table:"invoice-vm",client_table:"client-wapp"},
-        "invoice-print": 	        {url:"$H/m/invoice-print.html",Table:"invoice-vm", client_table:"client-wapp"},
-        "client-data": 	            {url:"$H/m/client-data-wapp.html",Table:"client-wapp",form_module:"client-form",router:1},
-        "client-form": 	            {url:"$H/m/client-form-wapp.html",Table:"client-wapp"},
-        "about-vmiis":  {url:"$H/m/about-vmiis.html",router:1},
+        "invoice-data": 	        {url:"$H/m/invoice-data.html",Table:"invoice-wappsystem",form_module:"invoice-form",router:1},
+        "invoice-form": 	        {url:"$H/m/invoice-form.html",Table:"invoice-wappsystem",client_table:"client-wappsystem"},
+        "invoice-print": 	        {url:"$H/m/invoice-print.html",Table:"invoice-wappsystem", client_table:"client-wappsystem"},
+        "client-data": 	            {url:"$H/m/client-data.html",Table:"client-wappsystem",form_module:"client-form",router:1},
+        "client-form": 	            {url:"$H/m/client-form.html",Table:"client-wappsystem"},
         "module-editor":{url:"https://vm.vmiis.com/component/m/module-editor.html",router:1,sys:1},
     }
     for(p in modules){
